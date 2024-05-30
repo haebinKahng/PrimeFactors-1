66896afd3f9ad6578ffbce5d7c00501ae2d640e5
Minyoung Kang <haebin1223.myk@gmail.com>
2024-05-30 13:38:40 +0900

    TDD Cycle 7

--- Project1/prime-factors.cpp
+++ Project1/prime-factors.cpp
@@ -5,18 +5,10 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> factors = {};
-		if (number > 1) {
-			int divisor = 2;
-            if (number == 4 || number == 6 || number == 9) {
-				for (divisor = 2; number > 1; divisor++) {
-					while (number % divisor == 0) {
-						factors.push_back(divisor);
-						number /= divisor;
-					}
-				}
-			}
-			else {
-				factors.push_back(number);
+		for (int divisor = 2; number > 1; divisor++) {
+			while (number % divisor == 0) {
+				factors.push_back(divisor);
+				number /= divisor;
 			}
 		}
 		return factors;
